379434c6f1856e9558dffe680d4eca60eee4b925
jin <jin@qq.com>
2019-07-22 15:07:45 +0800

    更新 7/22

--- src/pages/product/add-update.jsx
+++ src/pages/product/add-update.jsx
@@ -9,7 +9,7 @@ import {
   Button
 } from 'antd'
 
-import {reqCategorys} from '../../api'
+import { reqCategorys } from '../../api'
 import PicturesWall from './pictures-wall'
 import LinkButton from '../../components/link-button'
 import memoryUtils from '../../utils/memoryUtils';
@@ -37,36 +37,36 @@ class ProductAddUpdate extends Component {
   /* 
   对价格进行自定义验证
   */
- validatePrice = (rule, value, callback) => {
-    if (value==='') {
+  validatePrice = (rule, value, callback) => {
+    if (value === '') {
       callback()
-    } else if (value * 1 <=0) {
+    } else if (value * 1 <= 0) {
       callback('价格必须大于0')
     } else {
       callback()
     }
- }
+  }
 
- /* 
-  处理提交的回调
- */
- handleSubmit = (event) => {
-   // 阻止事件的默认行为(提交表单)
-   event.preventDefault()
+  /* 
+   处理提交的回调
+  */
+  handleSubmit = (event) => {
+    // 阻止事件的默认行为(提交表单)
+    event.preventDefault()
 
-   // 进行统一的表单验证
-   this.props.form.validateFields(async (err, values) => {
+    // 进行统一的表单验证
+    this.props.form.validateFields(async (err, values) => {
       if (!err) {
-        const {name, desc, price, categoryId} = values
+        const { name, desc, price, categoryId } = values
         console.log('发送请求', name, desc, price, categoryId)
-      } 
+      }
     })
- }
+  }
 
- componentWillMount () {
-   this.product = memoryUtils.product
-   this.isUpdate = !!this.product._id
- }
+  componentWillMount() {
+    this.product = memoryUtils.product
+    this.isUpdate = !!this.product._id
+  }
 
   componentDidMount() {
     this.getCategorys()
@@ -74,10 +74,10 @@ class ProductAddUpdate extends Component {
 
   render() {
     const { categorys } = this.state
-    const {isUpdate, product} = this
+    const { isUpdate, product } = this
 
     const { getFieldDecorator } = this.props.form
-   
+
     const title = (
       <span>
         <LinkButton onClick={() => this.props.history.goBack()}>
@@ -102,7 +102,7 @@ class ProductAddUpdate extends Component {
               rules: [
                 { required: true, message: '必须输入商品名称!' }
               ],
-            })(<Input placeholder="商品名称"/>)}
+            })(<Input placeholder="商品名称" />)}
           </Item>
           <Item label="商品描述">
             {getFieldDecorator('desc', {
@@ -110,7 +110,7 @@ class ProductAddUpdate extends Component {
               rules: [
                 { required: true, message: '必须输入商品描述!' }
               ],
-            })(<Input placeholder="商品描述"/>)}
+            })(<Input placeholder="商品描述" />)}
           </Item>
           <Item label="商品价格">
             {getFieldDecorator('price', {
@@ -119,7 +119,7 @@ class ProductAddUpdate extends Component {
                 { required: true, message: '必须输入价格!' },
                 { validator: this.validatePrice }
               ],
-            })(<Input type="number" placeholder="商品价格" addonAfter="元"/>)}
+            })(<Input type="number" placeholder="商品价格" addonAfter="元" />)}
           </Item>
           <Item label="商品分类">
             {getFieldDecorator('categoryId', {
@@ -145,7 +145,7 @@ class ProductAddUpdate extends Component {
           <Item>
             <Button type="primary" htmlType="submit">提交</Button>
           </Item>
-       </Form>
+        </Form>
       </Card>
     )
   }
